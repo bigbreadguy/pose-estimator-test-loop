f4a2011579fe85cc69ba051f2ac01dddd6326c6f
bigbreadguy <50568142+bigbreadguy@users.noreply.github.com>
2021-11-16 14:12:31 +0900

    Update loop.py
    a method for designated training loop is added.

--- src/loop.py
+++ src/loop.py
@@ -111,33 +111,42 @@ class TestLoop(object):
                 # Initiate test loop
                 test(args=self.args)
 
-    """
-    def train(self):
-        # Set arguments for training
-        self.vars["mode"] = "train"
+    def train_spec(self):
+        design = args.spec
+        setups_dir = os.path.join(self.datasets_dir, design)
+        if os.path.exists(setups_dir):
+            reports_dir = os.path.join(self.test_report_dir, design)
+            if not os.path.exists(reports_dir):
+                os.makedirs(reports_dir)
 
-        self.vars["log_prefix"] = os.path.join(self.execute_log_dir, setup)
+            setups_list = os.listdir(setups_dir)
+            for idx_s, setup in enumerate(setups_list):
+                # Set arguments for training
+                self.vars["mode"] = "train"
 
-        train_data_dir = os.path.join(setups_dir, setup)
-        if not os.path.exists(train_data_dir):
-            os.makedirs(train_data_dir)
-        self.vars["data_dir"] = train_data_dir
+                self.vars["log_prefix"] = os.path.join(self.execute_log_dir, setup)
 
-        with open(os.path.join(train_data_dir, "train", "labels", "mpii_style.json"), "r", encoding="utf-8") as fread:
-            labels_dict = json.load(fread)
-            num_mark = len(labels_dict[0]["joints_vis"])
-        self.vars["num_mark"] = num_mark
+                train_data_dir = os.path.join(setups_dir, setup)
+                if not os.path.exists(train_data_dir):
+                    os.makedirs(train_data_dir)
+                self.vars["data_dir"] = train_data_dir
 
-        num_epoch = self.args.num_epoch
-        
-        train_report_dir = os.path.join(reports_dir, design, setup)
-        if not os.path.exists(train_report_dir):
-            os.makedirs(train_report_dir)
-        
-        self.args.ckpt_dir = os.path.join(train_report_dir, "checkpoint")
-        self.args.log_dir = os.path.join(train_report_dir, "log")
-        self.args.result_dir = os.path.join(train_report_dir, "result")
-        
-        # Initiate train loop
-        train(args=self.args)
-        """
+                with open(os.path.join(train_data_dir, "train", "labels", "mpii_style.json"), "r", encoding="utf-8") as fread:
+                    labels_dict = json.load(fread)
+                    num_mark = len(labels_dict[0]["joints_vis"])
+                self.vars["num_mark"] = num_mark
+
+                num_epoch = self.args.num_epoch
+                
+                train_report_dir = os.path.join(reports_dir, design, setup)
+                if not os.path.exists(train_report_dir):
+                    os.makedirs(train_report_dir)
+                
+                self.args.ckpt_dir = os.path.join(train_report_dir, "checkpoint")
+                self.args.log_dir = os.path.join(train_report_dir, "log")
+                self.args.result_dir = os.path.join(train_report_dir, "result")
+                
+                # Initiate train loop
+                train(args=self.args)
+        else:
+            print(f"Designated Spec {design} Does not Exists!")
