40b79d0acd83ffb4bb49d0d58a542df723500836
bigbreadguy <50568142+bigbreadguy@users.noreply.github.com>
2021-11-16 14:13:16 +0900

    Update main.py
    main script updated to support designated training

--- main.py
+++ main.py
@@ -9,11 +9,15 @@ if __name__ == "__main__":
     print("Building Test Loop")
     loop.build()
     
-    if args.loop == "stroll":
-        print("Strolling Test Loop")
-        loop.stroll()
-    elif args.loop == "test":
-        print("Test Only")
-        loop.test_stroll()
+    if args.spec == "all":
+        if args.loop == "stroll":
+            print("Strolling Test Loop")
+            loop.stroll()
+        elif args.loop == "test":
+            print("Test Only")
+            loop.test_stroll()
+    else:
+        print(f"Training Designated Spec : {args.spec}")
+        loop.train_spec()
 
     print("Done")
