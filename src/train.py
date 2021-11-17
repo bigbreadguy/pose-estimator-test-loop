e7871ed39e35d6131f79eee7bbef0c4292895a3d
bigbreadguy <schadenlim@gmail.com>
2021-11-17 19:22:30 +0900

    Update train.py
    output and target are happened to be numpy arrays, not tensors.

--- src/train.py
+++ src/train.py
@@ -380,10 +380,8 @@ def test(args):
                         plt.imsave(os.path.join(result_dir_test, '%04d_output.png' % id), output_)
                         plt.imsave(os.path.join(result_dir_test, '%04d_target.png' % id), target_)
                         writer_test.add_image('input', input_data, id, dataformats='NHWC')
-                        output = tensor2image(output)
-                        writer_test.add_image('output', output, id, dataformats='NHWC')
-                        target = tensor2image(target)
-                        writer_test.add_image('target', target, id, dataformats='NHWC')
+                        writer_test.add_image('output', output_, id, dataformats='NHWC')
+                        writer_test.add_image('target', target_, id, dataformats='NHWC')
 
                         f.write("TEST: BATCH %04d / %04d | " % (id + 1, num_data_test))
                 else:
@@ -405,10 +403,8 @@ def test(args):
                     plt.imsave(os.path.join(result_dir_test, '%04d_output.png' % id), output_)
                     plt.imsave(os.path.join(result_dir_test, '%04d_target.png' % id), target_)
                     writer_test.add_image('input', input_data, id, dataformats='NHWC')
-                    output = tensor2image(output)
-                    writer_test.add_image('output', output, id, dataformats='NHWC')
-                    target = tensor2image(target)
-                    writer_test.add_image('target', target, id, dataformats='NHWC')
+                    writer_test.add_image('output', output_, id, dataformats='NHWC')
+                    writer_test.add_image('target', target_, id, dataformats='NHWC')
 
                     f.write("TEST: BATCH %04d / %04d | " % (id + 1, num_data_test))
 
