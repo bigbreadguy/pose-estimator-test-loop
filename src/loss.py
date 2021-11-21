a4ecccfd2ff8e3c63c4557d231bab88e02633b63
bigbreadguy <schadenlim@gmail.com>
2021-11-22 08:42:56 +0900

    Update loss.py
    creat PCKhLoss, a loss metric that evaluate PCKh to optimize the model over the metric

--- src/loss.py
+++ src/loss.py
@@ -10,7 +10,9 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+from src.evaluate import accuracy
 
+import torch
 import torch.nn as nn
 
 class JointsMSELoss(nn.Module):
@@ -39,3 +41,109 @@ class JointsMSELoss(nn.Module):
                 loss += 0.5 * self.criterion(heatmap_pred, heatmap_gt)
 
         return loss / num_joints
+
+class PCKhLoss(nn.Module):
+    def __init__(self, thr, hm_type='gaussian'):
+        super(PCKhLoss, self).__init__()
+        self.thr = thr
+        self.hm_type = hm_type
+
+        self.criterion = nn.MSELoss(reduction="mean")
+
+    def get_max_preds(batch_heatmaps):
+        '''
+        get predictions from score maps
+        heatmaps: numpy.ndarray([batch_size, num_joints, height, width])
+        '''
+        assert isinstance(batch_heatmaps, torch.Tensor), \
+            'batch_heatmaps should be numpy.ndarray'
+        assert batch_heatmaps.ndim == 4, 'batch_images should be 4-ndim'
+
+        batch_size = batch_heatmaps.shape[0]
+        num_joints = batch_heatmaps.shape[1]
+        width = batch_heatmaps.shape[3]
+        heatmaps_reshaped = batch_heatmaps.reshape((batch_size, num_joints, -1))
+        idx = torch.argmax(nn.Softmax(heatmaps_reshaped), 2)
+        maxvals = torch.amax(nn.Softmax(heatmaps_reshaped), 2)
+
+        maxvals = maxvals.reshape((batch_size, num_joints, 1))
+        idx = idx.reshape((batch_size, num_joints, 1))
+
+        preds = torch.tile(idx, (1, 1, 2))
+
+        preds[:, :, 0] = (preds[:, :, 0]) % width
+        preds[:, :, 1] = torch.floor((preds[:, :, 1]) / width)
+
+        pred_mask = torch.tile(torch.gt(maxvals, 0.0), (1, 1, 2))
+
+        preds *= pred_mask
+        return preds, maxvals
+
+    def calc_dists(self, preds, target, normalize):
+        """
+        pyTorch automatically convert
+        integers into single precision floats
+        """
+        # preds = preds.astype(np.float32)
+        # target = target.astype(np.float32)
+        dists = torch.zeros((preds.shape[1], preds.shape[0]))
+        for n in range(preds.shape[0]):
+            for c in range(preds.shape[1]):
+                if target[n, c, 0] > 1 and target[n, c, 1] > 1:
+                    normed_preds = preds[n, c, :] / normalize[n]
+                    normed_targets = target[n, c, :] / normalize[n]
+                    dists[c, n] = torch.linalg.norm(normed_preds - normed_targets)
+                else:
+                    dists[c, n] = -1
+        return dists
+
+
+    def dist_acc(self, dists):
+        ''' Return percentage below threshold while ignoring values with a -1 '''
+        dist_cal = torch.ne(dists, -1)
+        num_dist_cal = dist_cal.sum()
+        if num_dist_cal > 0:
+            return torch.lt(dists[dist_cal], self.thr).sum() * 1.0 / num_dist_cal
+        else:
+            return -1
+
+
+    def accuracy(self, output, target):
+        '''
+        Calculate accuracy according to PCK,
+        but uses ground truth heatmap rather than x,y locations
+        First value to be returned is average accuracy across 'idxs',
+        followed by individual accuracies
+        '''
+        idx = list(range(output.shape[1]))
+        norm = 1.0
+        if self.hm_type == 'gaussian':
+            pred, _ = self.get_max_preds(output)
+            target, _ = self.get_max_preds(target)
+            h = output.shape[2]
+            w = output.shape[3]
+            norm = torch.ones((pred.shape[0], 2)) * torch.Tensor([h, w]) / 10
+        dists = self.calc_dists(pred, target, norm)
+
+        acc = torch.zeros((len(idx) + 1))
+        avg_acc = 0
+        cnt = 0
+
+        for i in range(len(idx)):
+            acc[i + 1] = self.dist_acc(dists[idx[i]])
+            if acc[i + 1] >= 0:
+                avg_acc = avg_acc + acc[i + 1]
+                cnt += 1
+
+        avg_acc = avg_acc / cnt if cnt != 0 else 0
+        if cnt != 0:
+            acc[0] = avg_acc
+        return acc, avg_acc, cnt, pred
+    
+    def forward(self, output, target):
+        acc, avg_acc, cnt, pred = self.accuracy(output, target)
+        grnd_t = torch.ones_like(acc)
+        
+        del cnt, pred
+        
+        return (self.criterion(acc, grnd_t), avg_acc)
